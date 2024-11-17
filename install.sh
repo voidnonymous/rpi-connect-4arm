11e3dc3b55fe9904436d7aae2bd3b873a0510bb5
voidnonymous <90940293+voidnonymous@users.noreply.github.com>
2024-11-17 16:40:35 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-
+echo "Downloading the .deb file"
+wget https://github.com/voidnonymous/rpi-connect-4arm/raw/refs/heads/main/rpi-connect_2.1.0_arm64.deb
+echo "Renaming file"
+mv rpi-connect_2.1.0_arm64.deb rpi-connect-arm64.deb
+echo "Installing file (You need sudo for this to work!)"
+sudo dpkg -i rpi-connect-arm64.deb
